4c8c18cdae6ab16b5d0122cd82a288a06f7f9e57
dedsec <mi-pacman@megacorp.industries>
2022-07-22 12:58:55 +0930

    Added M.I MOTD

--- stacks/rapture-fullstack_0.1.0/scripts/setup-arch.sh
+++ stacks/rapture-fullstack_0.1.0/scripts/setup-arch.sh
@@ -9,14 +9,18 @@ systemctl enable systemd-timesyncd  # Enable timesync daemon on boot
 echo "vagrant:changeme" | chpasswd  # Change vagrants password to changeme
 cp /vagrant/configs/bash_aliases /home/vagrant/.bash_aliases
 
+######################
+###PACKAGE_INSTALLS###
+######################
+pacman -Sy  # Update repos
+pacman -S --noconfirm docker docker-compose  # Install docker and compose
+
 ##########################
 ###DOCKER_CONFIGURATION###
 ##########################
-#pacman -Sy  # Update repos
-#pacman -S --noconfirm docker docker-compose  # Update repositories, install docker and compose
-#systemctl enable docker && systemctl start docker  # Enable and start docker daemon on boot
-#docker pull midockerdb/terraform-controller:0.1.3  # Pull custom terraform container image
-#docker pull midockerdb/packer-controller:0.1.3  # Pull custom packer container image
+systemctl enable docker && systemctl start docker  # Enable and start docker daemon on boot
+docker pull midockerdb/terraform-controller:0.1.3  # Pull custom terraform container image
+docker pull midockerdb/packer-controller:0.1.3  # Pull custom packer container image
 
 #########################
 ###CLOUD_CONFIGURATION###
@@ -28,7 +32,7 @@ chown -R vagrant:vagrant /home/vagrant
 ############################
 ###PERSONAL_CUSTOMIZATION###
 ############################
-cp /vagrant/scripts/update_motd.sh /usr/bin/update_motd.sh
-#chmod 775 /usr/bin/update_motd.sh
-#printf 'session   optional   pam_exec.so   stdout /usr/bin/update_motd.sh\nsession   optional   pam_motd.so   motd=/etc/motd' >> /etc/pam.d/sshd
-reboot
+cp /vagrant/scripts/update_motd.sh /usr/bin/update_motd.sh  # Copy MOTD script to machine
+chmod 775 /usr/bin/update_motd.sh  # Set permissions on MOTD script
+printf 'session   optional   pam_exec.so   stdout /usr/bin/update_motd.sh\nsession   optional   pam_motd.so   motd=/etc/motd' >> /etc/pam.d/sshd  # Set PAM to execute script upon each new session
+sed -i 's/session    optional   pam_motd.so/#session    optional   pam_motd.so/g' /etc/pam.d/system-login  # Prevent MOTD from multiplying upon each ssh session
