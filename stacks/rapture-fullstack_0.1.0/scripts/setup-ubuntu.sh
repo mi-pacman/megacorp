04c75c5123db28f7af5f62526b2ee3604a0bc313
dedsec <mi-pacman@megacorp.industries>
2022-07-23 08:30:58 +0930

    fixed permissions on setup-ubuntu.sh

--- stacks/rapture-fullstack_0.1.0/scripts/setup-ubuntu.sh
+++ stacks/rapture-fullstack_0.1.0/scripts/setup-ubuntu.sh
@@ -43,15 +43,15 @@ git clone https://github.com/mi-pacman/megacorp-proxy /home/terraform
 ###PERSONAL_CUSTOMIZATION###
 ############################
 
-cp /vagrant/scripts/update_motd.sh /usr/bin/update_motd.sh  # Copy MOTD script to machine
+sudo cp /vagrant/scripts/update_motd.sh /usr/bin/update_motd.sh  # Copy MOTD script to machine
 
 chmod 775 /usr/bin/update_motd.sh  # Set permissions on MOTD script
 
-sed -i 's;session    optional     pam_motd.so  motd=/etd/motd;#session    optional     pam_motd.so  motd=/etc/motd;g' /etc/pam.d/sshd  # Set PAM to execute script upon each new session
+sudo sed -i 's;session    optional     pam_motd.so  motd=/etd/motd;#session    optional     pam_motd.so  motd=/etc/motd;g' /etc/pam.d/sshd  # Set PAM to execute script upon each new session
 
-sed -i 's;session    optional     pam_motd.so noupdate;#session    optional     pam_motd.so noupdate;g' /etc/pam.d/sshd  # Prevent MOTD from showing twice
+sudo sed -i 's;session    optional     pam_motd.so noupdate;#session    optional     pam_motd.so noupdate;g' /etc/pam.d/sshd  # Prevent MOTD from showing twice
 
-printf 'session   optional   pam_exec.so   stdout /usr/bin/update_motd.sh\nsession   optional   pam_motd.so   motd=/etc/motd' >> /etc/pam.d/sshd  # Set PAM to execute script upon each new session
+sudo printf 'session   optional   pam_exec.so   stdout /usr/bin/update_motd.sh\nsession   optional   pam_motd.so   motd=/etc/motd' >> /etc/pam.d/sshd  # Set PAM to execute script upon each new session
 
 
 
