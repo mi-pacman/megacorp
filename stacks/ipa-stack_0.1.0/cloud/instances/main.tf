10a227c2b7cbe16f712844cb5d8dd01f250c7f9f
dedsec <mi-pacman@megacorp.industries>
2022-07-23 21:10:30 +0930

    Got a working base for cloud ipa server

--- stacks/ipa-stack_0.1.0/cloud/instances/main.tf
+++ stacks/ipa-stack_0.1.0/cloud/instances/main.tf
@@ -45,7 +45,7 @@ resource "aws_security_group" "sg_22" {
   name   = "sg_22"
   vpc_id = aws_vpc.vpc.id
 
-  # SSH access from the VPC
+  # TCP SSH access from the VPC
   ingress {
     from_port   = 22
     to_port     = 22
@@ -65,10 +65,77 @@ resource "aws_security_group" "sg_80" {
   name   = "sg_80"
   vpc_id = aws_vpc.vpc.id
 
-  # HTTP access from the VPC
+  # TCP HTTP access from the VPC
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "sg_88" {
+  name   = "sg_88"
+  vpc_id = aws_vpc.vpc.id
+
+  # TCP & UDP Kerberos access from the VPC
+  ingress {
+    from_port   = 88
+    to_port     = 88
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 88
+    to_port     = 88
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "sg_123" {
+  name   = "sg_123"
+  vpc_id = aws_vpc.vpc.id
+
+  # UDP NTP access from the VPC
+  ingress {
+    from_port   = 123
+    to_port     = 123
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "sg_389" {
+  name   = "sg_389"
+  vpc_id = aws_vpc.vpc.id
+
+  # TCP LDAP access from the VPC
+  ingress {
+    from_port   = 389
+    to_port     = 389
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -85,7 +152,7 @@ resource "aws_security_group" "sg_443" {
   name   = "sg_443"
   vpc_id = aws_vpc.vpc.id
 
-  # HTTPS access from the VPC
+  # TCP HTTPS access from the VPC
   ingress {
     from_port   = 443
     to_port     = 443
@@ -101,24 +168,63 @@ resource "aws_security_group" "sg_443" {
   }
 }
 
-resource "aws_instance" "web" {
-  ami                         = "<AMI>"
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.subnet_public.id
-  vpc_security_group_ids      = [aws_security_group.sg_22.id, aws_security_group.sg_80.id, aws_security_group.sg_443.id]
-  associate_public_ip_address = true
+resource "aws_security_group" "sg_464" {
+  name   = "sg_464"
+  vpc_id = aws_vpc.vpc.id
 
-  tags = {
-    Name = "cloud-controller"
+  # TCP Kerberos access from the VPC
+  ingress {
+    from_port   = 464
+    to_port     = 464
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_route53_record" "domain" {
-  zone_id = "<ZONE_ID>" 
-  name    = "<DOMAIN_NAME>"
-  type    = "A"
-  ttl     = "5"
-  records = [aws_instance.web.public_ip]
+resource "aws_security_group" "sg_636" {
+  name   = "sg_636"
+  vpc_id = aws_vpc.vpc.id
+
+  # TCP & UDP LDAPS Kerberos access from the VPC
+  ingress {
+    from_port   = 636
+    to_port     = 636
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 636
+    to_port     = 636
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                         = "<AMI>"
+  instance_type               = "t2.small"
+  subnet_id                   = aws_subnet.subnet_public.id
+  vpc_security_group_ids      = [aws_security_group.sg_22.id, aws_security_group.sg_80.id, aws_security_group.sg_88.id, aws_security_group.sg_123.id, aws_security_group.sg_389.id, aws_security_group.sg_443.id, aws_security_group.sg_464.id, aws_security_group.sg_636.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "ipa-controller"
+  }
 }
 
 output "public_ip" {
